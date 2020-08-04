4a0a91a7b937a933e3a3c701db0e0377579f853f
Ed Randall <edrandall@macbook-wired.thelinuxnetwork.com>
2020-08-04 08:45:51 +0100

    added new security policy script

--- create-env.sh
+++ create-env.sh
@@ -10,9 +10,12 @@ echo
 echo "List of provisioned DB instances: "
 gcloud beta sql instances list
 
+echo
+
 echo -ne "The Load Balancer's external IP address is: "
 gcloud compute addresses describe lb-ipv4-1 --format="get(address)" --global
 
+echo
 echo "The following records are set in public DNS:"
 host edrandall.uk | grep has
 host www.edrandall.uk | grep has
---
+++ sec-pol.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Source the global variables
+. ./0-gcp-global-vars.sh
+
+#Ensure we're working with the correct project
+runprint \
+    gcloud config set project $PROJECT_NAME
+sleep 1
+
+runprint \
+    gcloud compute security-policies create $PROJECT_NAME-lb-policy
+
+runprint \
+    gcloud compute backend-services update web-backend-service \
+    --security-policy=$PROJECT_NAME-lb-policy \
+    --global
+
+runprint \
+    gcloud compute security-policies rules create 3 \
+    --security-policy $PROJECT_NAME-lb-policy \
+    --expression "!inIpRange(origin.ip, '82.22.123.140/32')" \
+    --action "deny-404"
+
+# runprint \
+#     gcloud compute security-policies rules create 1 \
+#     --security-policy $PROJECT_NAME-lb-policy \
+#     --expression "!inIpRange(origin.ip, '82.22.123.140/32') && request.path.matches('/wp-admin/')" \
+#     --action "deny-404"
+
+# runprint \
+#     gcloud compute security-policies rules create 9999 \
+#     --security-policy=$PROJECT_NAME-lb-policy \
+#     --description="Default rule, higher priority overrides it" \
+#     --src-ip-ranges=\* \
+#     --action=deny-403 
